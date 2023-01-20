f9068862445c3fd5ffa5ace9f700bfc5058f8d93
chrg <christian.ricci2000@gmail.com>
2023-01-21 00:53:46 +0100

    Add more methods and stuff to other array classes.
    These changes were done to be more in line with standard containers.

--- array.hpp
+++ array.hpp
@@ -1,101 +1,157 @@
 #pragma once
 
 #include <cstddef>
-#include <cstring>
-#include <cassert>
-#include <array>
 #include <span>
 #include <memory>
 
 namespace util {
 
+/* An array with size determined at run-time. This size never grows.  */
 template <typename T>
 class HeapArray {
-    std::unique_ptr<T[]> ptr;
-    std::size_t len = 0;
+    std::unique_ptr<T[]> p;
+    std::size_t l = 0;
 
 public:
-    using iterator = T *;
+    using value_type      = T;
+    using size_type       = std::size_t;
+    using difference_type = std::ptrdiff_t;
+    using reference       =       value_type &;
+    using const_reference = const value_type &;
+    using pointer         =       value_type *;
+    using const_pointer   = const value_type *;
+    using iterator        =       value_type *;
+    using const_iterator  = const value_type *;
 
-    HeapArray() = default;
-    explicit HeapArray(std::size_t s)           { reset(s); }
-          T & operator[](std::size_t pos)       { return ptr[pos]; }
-    const T & operator[](std::size_t pos) const { return ptr[pos]; }
-          T *data()       noexcept              { return ptr.get(); }
-    const T *data() const noexcept              { return ptr.get(); }
-          iterator begin()       noexcept       { return ptr.get(); }
-    const iterator begin() const noexcept       { return ptr.get(); }
-          iterator end()                        { return ptr.get() + len; }
-    const iterator end()   const noexcept       { return ptr.get() + len; }
-    [[nodiscard]] bool empty() const noexcept   { return len == 0; }
-    std::size_t size() const noexcept           { return len; }
-
-    void reset(std::size_t s)
+    constexpr HeapArray() = default;
+    constexpr HeapArray(size_type count, const T &value = T())
+        : p(std::make_unique<T[]>(count)), l(count)
     {
-        ptr = std::make_unique<T[]>(s);
-        len = s;
+        for (auto i = 0u; i < l; i++)
+            p[i] = value;
     }
+
+    template <typename It>
+    constexpr HeapArray(It first, It last)
+        : p(std::make_unique<T[]>(last - first)), l(last - first)
+    {
+        for (auto i = 0u; i < l; i++)
+            p[i] = *(first + i);
+    }
+
+    constexpr HeapArray(std::initializer_list<T> init)
+        : HeapArray(init.begin(), init.end())
+    { }
+
+    constexpr       reference operator[](size_type pos)                { return p[pos]; }
+    constexpr const_reference operator[](size_type pos) const          { return p[pos]; }
+    constexpr       reference front()                                  { return p[0]; }
+    constexpr const_reference front()                   const          { return p[0]; }
+    constexpr       reference back()                                   { return p[l-1]; }
+    constexpr const_reference back()                    const          { return p[l-1]; }
+    constexpr              T *data()                          noexcept { return p.get(); }
+    constexpr        const T *data()                    const noexcept { return p.get(); }
+    constexpr        iterator begin()                         noexcept { return p.get(); }
+    constexpr  const_iterator begin()                   const noexcept { return p.get(); }
+    constexpr  const_iterator cbegin()                  const noexcept { return p.get(); }
+    constexpr        iterator end()                           noexcept { return p.get()+l; }
+    constexpr  const_iterator end()                     const noexcept { return p.get()+l; }
+    constexpr  const_iterator cend()                    const noexcept { return p.get()+l; }
+    [[nodiscard]]
+    constexpr            bool empty()                   const noexcept { return l == 0; }
+    constexpr       size_type size()                    const noexcept { return l; }
 };
 
+/* Just like std::array, but in two dimensions. Doesn't implement iterators. */
 template <typename T, std::size_t Width, std::size_t Height>
 class Array2D {
     static_assert(Width != 0 && Height != 0, "Can't define an Array2D with 0 width or height");
     T arr[Width*Height];
 
 public:
-    constexpr std::span<T>       operator[](std::size_t pos)       { return std::span{ arr+pos*Width, Width }; }
-    constexpr std::span<const T> operator[](std::size_t pos) const { return std::span{ arr+pos*Width, Width }; }
+    using value_type      = T;
+    using size_type       = std::size_t;
+    using difference_type = std::ptrdiff_t;
+    using reference       = std::span<T>;
+    using const_reference = std::span<const T>;
 
-    constexpr T *data() { return arr; }
-    constexpr const T *data() const { return arr; }
-    constexpr std::size_t width()  const { return Width; }
-    constexpr std::size_t height() const { return Height; }
+    constexpr       reference operator[](size_type pos)                { return std::span{ arr+pos*Width, Width }; }
+    constexpr const_reference operator[](size_type pos) const          { return std::span{ arr+pos*Width, Width }; }
+    constexpr       reference front()                                  { return operator[](0); }
+    constexpr const_reference front()                   const          { return operator[](0); }
+    constexpr       reference back()                                   { return operator[](Height-1); }
+    constexpr const_reference back()                    const          { return operator[](Height-1); }
+    constexpr              T *data()                          noexcept { return arr; }
+    constexpr        const T *data()                    const noexcept { return arr; }
+    [[nodiscard]]
+    constexpr      bool empty()                         const noexcept { return Height == 0 || Width == 0; }
+    constexpr size_type width()                         const noexcept { return Width; }
+    constexpr size_type height()                        const noexcept { return Height; }
 };
 
+/* A statically allocated vector. Its max size is always equal to N. */
 template <typename T, unsigned N>
 class StaticVector {
-    T arr[N];
-    T *cur = arr;
+    T p[N];
+    T *c = p;
 
 public:
+    using value_type      = T;
+    using size_type       = std::size_t;
+    using difference_type = std::ptrdiff_t;
+    using reference       =       value_type &;
+    using const_reference = const value_type &;
+    using pointer         =       value_type *;
+    using const_pointer   = const value_type *;
+    using iterator        =       value_type *;
+    using const_iterator  = const value_type *;
+
     constexpr StaticVector() = default;
 
-    constexpr       T & operator[](std::size_t pos)       { return arr[pos]; }
-    constexpr const T & operator[](std::size_t pos) const { return arr[pos]; }
-
-    constexpr       T *data()        noexcept { return arr; }
-    constexpr const T *data()  const noexcept { return arr; }
-
-    using iterator = T *;
-
-    constexpr       iterator begin()       noexcept { return arr; }
-    constexpr const iterator begin() const noexcept { return arr; }
-    constexpr       iterator end()         noexcept { return cur; }
-    constexpr const iterator end()   const noexcept { return cur; }
-
-    [[nodiscard]] constexpr bool empty() const noexcept { return cur == arr; }
-    constexpr std::size_t size() const noexcept { return cur - arr; }
-
-    constexpr void clear() noexcept          { cur = arr; }
-    constexpr void push_back(const T &value) { *cur++ = value; }
-    constexpr void push_back(T &&value)      { *cur++ = std::move(value); }
-    constexpr void pop_back()                { cur--; }
+    constexpr       reference operator[](size_type pos)                { return p[pos]; }
+    constexpr const_reference operator[](size_type pos) const          { return p[pos]; }
+    constexpr       reference front()                                  { return p[0]; }
+    constexpr const_reference front()                   const          { return p[0]; }
+    constexpr       reference back()                                   { return c[-1]; }
+    constexpr const_reference back()                    const          { return c[-1]; }
+    constexpr              T *data()                          noexcept { return p; }
+    constexpr        const T *data()                    const noexcept { return p; }
+    constexpr       iterator begin()                          noexcept { return p; }
+    constexpr const_iterator begin()                    const noexcept { return p; }
+    constexpr       iterator end()                            noexcept { return c; }
+    constexpr const_iterator end()                      const noexcept { return c; }
+    [[nodiscard]]
+    constexpr           bool empty()                    const noexcept { return c == p; }
+    constexpr      size_type size()                     const noexcept { return c - p; }
+    constexpr           void clear() noexcept                          { c = p; }
+    constexpr           void push_back(const T &value)                 { *c++ = value; }
+    constexpr           void push_back(T &&value)                      { *c++ = std::move(value); }
+    constexpr           void pop_back()                                { c--; }
 
     template <typename... Args>
-    constexpr T & emplace_back(Args&&... args)
+    constexpr reference emplace_back(Args&&... args)
     {
-        T *ptr = new (cur) T(args...);
-        cur++;
-        return *ptr;
+        new (c) T(args...);
+        c++;
+        return c[-1];
+    }
+
+    constexpr void resize(size_type count, T value = T())
+    {
+        auto diff = count - size();
+        for (auto *ptr = c; ptr < c + diff; ptr++)
+            *ptr = value;
+        c += diff;
     }
 };
 
 /* A 2D view over a 1D array. */
 template <typename T>
-struct Span2D {
+class Span2D {
     T *d;
     std::size_t w, h, s;
 
+public:
     constexpr Span2D() : d(nullptr), w(0), h(0), s(0) {}
     constexpr Span2D(T *data, std::size_t width, std::size_t height, std::size_t stride)
         : d(data), w(width), h(height), s(stride)
