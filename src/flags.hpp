7956762158462a7fbe2d17b42fa6c0df8675a60d
chrg <christian.ricci2000@gmail.com>
2023-06-04 16:51:11 +0200

    Add flags.hpp.
    flags.hpp is a new small library for using flags. This way we avoid the
    code bloat any time we want to use flags for... well, anything. I always
    found that pretty stupid.

---
+++ src/flags.hpp
@@ -0,0 +1,22 @@
+#include <type_traits>
+#include <bit>
+#include "common.hpp"
+
+template <typename T>
+class Flags {
+    using NumType = u64;
+    NumType data = 0;
+public:
+    explicit Flags() = default;
+    explicit Flags(auto&&... values)      { (add(values), ...); }
+    void add(T value)                     { data |=  (1 << static_cast<NumType>(value)); }
+    void remove(T value)                  { data &= ~(1 << static_cast<NumType>(value)); }
+    bool contains(T value) const          { return data & (1 << static_cast<NumType>(value)); }
+    int count()            const          { return std::popcount(data); }
+    NumType value()        const          { return data; }
+    Flags<T> & unite(Flags<T> &other)     { data |= other.data; return *this; }
+    Flags<T> & intersect(Flags<T> &other) { data &= other.data; return *this; }
+    explicit operator bool() const        { return data; }
+};
+
+template <typename T> Flags<T> operator|(T a, T b) { return Flags<T> { a, b }; }
